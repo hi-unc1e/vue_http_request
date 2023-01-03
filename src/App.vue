68f38a5da874857864a0bd9bb6af609b78a2d3d6
hi-unc1e <root@zuoxueba.org>
2023-01-03 22:35:35 +0800

    layout: update position

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
   <br>
-  1
-  <br>
-        <RequestMethodButton />
-  <hr>
-  <br>
-  11
-  <br>
 
-        <UrlInputBox />
+  <a-divider orientation="left">Postman</a-divider>
+  <a-row type="flex">
+    <a-col flex="100px">
+      <RequestMethodButton/>
+    </a-col>
+    <a-col flex="300px">
+      <UrlInputBox/>
+    </a-col>
+    <a-col flex="auto">
+      <a-button type="primary" @click="goClick()">Go</a-button>
+      <a-button>cancel</a-button>
+
+    </a-col>
+
+  </a-row>
+
+
+  <hr>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 import UrlInputBox from "./UrlInputBox.vue";
 import RequestMethodButton from "./RequestMethodButton.vue";
 
 
 export default defineComponent({
-components: {
-  UrlInputBox,
-  RequestMethodButton
-},
+  components: {
+    UrlInputBox,
+    RequestMethodButton
+  },
+  methods: {
+    goClick
+  }
 });
+
+export function goClick() {
+  alert("cyh 233")
+}
 </script>
